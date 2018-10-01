c1c4b9d33088b040865080837a1cff32b66c4be0
mrcracken <32810914+mrcracken@users.noreply.github.com>
2018-10-01 22:23:21 +0300

    Update lab1_task3.py

--- lab1/lab1_task3.py
+++ lab1/lab1_task3.py
@@ -1,48 +1,11 @@
 from numpy import linalg as la
 
-def define_Hilbert(n):
-    for i in range(1,n+1):
-        for j in range(1,n+1):
-            A[i][j] = 1/(i+j-1)
-    return A
-
-la.cholesky(define_Hilbert(4))
-
-def nearestPD(A):
-    """    
-    https://gist.github.com/fasiha/fdb5cec2054e6f1c6ae35476045a0bbd
+def hilbert_matrix(n):
     """
-
-    B = (A + A.T) / 2
-    _, s, V = la.svd(B)
-
-    H = np.dot(V.T, np.dot(np.diag(s), V))
-
-    A2 = (B + H) / 2
-
-    A3 = (A2 + A2.T) / 2
-
-    if isPD(A3):
-        return A3
-
-    spacing = np.spacing(la.norm(A))
-    # The above is different from [1]. It appears that MATLAB's `chol` Cholesky
-    # decomposition will accept matrixes with exactly 0-eigenvalue, whereas
-    # Numpy's will not. So where [1] uses `eps(mineig)` (where `eps` is Matlab
-    # for `np.spacing`), we use the above definition. CAVEAT: our `spacing`
-    # will be much larger than [1]'s `eps(mineig)`, since `mineig` is usually on
-    # the order of 1e-16, and `eps(1e-16)` is on the order of 1e-34, whereas
-    # `spacing` will, for Gaussian random matrixes of small dimension, be on
-    # othe order of 1e-16. In practice, both ways converge, as the unit test
-    # below suggests.
-    I = np.eye(A.shape[0])
-    k = 1
-    while not isPD(A3):
-        mineig = np.min(np.real(la.eigvals(A3)))
-        A3 += I * (-mineig * k**2 + spacing)
-        k += 1
-
-    return A3
+    https://stackoverflow.com/questions/6667201/how-to-define-a-two-dimensional-array-in-python
+    """
+    Matrix = [[1/(i+j-1) for i in range(1,n+1)] for j in range(1,n+1)] 
+    return Matrix
 
 def isPD(B):
     """
@@ -50,17 +13,18 @@ def isPD(B):
     """
     try:
         _ = la.cholesky(B)
-        print(_)
         return True
     except la.LinAlgError:
         return False
 
 if __name__ == '__main__':
-    import numpy as np
-    for i in range(10):
-        for j in range(2, 100):
-            A = np.random.randn(j, j)
-            # Error is here. Fuck it!
-            B = nearestPD(define_Hilbert(4))
-            assert(isPD(B))
-    print('unit test passed!')
+    n = 4
+    M = hilbert_matrix(n)
+    try:
+        isPD(M)
+        B = la.cholesky(hilbert_matrix(4))
+        Det = la.linalg.det(M)
+        print("Cholesky: \n" , B)
+        print("\n DET of Hilbert matrix = " , Det)
+    except la.LinAlgError:
+        print("Error")
